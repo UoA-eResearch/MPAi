7f9def23328300115548204762a8a1e08b2ceca8
Noel <noel.zeng@auckland.ac.nz>
2024-09-10 17:30:55 +1200

    Minor amend for acknowledgements

--- pages/WelcomePage.js
+++ pages/WelcomePage.js
@@ -55,8 +55,8 @@ export default {
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-      <p>The Māori Pronunciation Aid project is led by Professor Catherine Watson, Dr Justine Hui, and Dr Peter Keegan from Waipapa Taumata Rau, the University of Auckland.</p>
-      <p>The JavaScript app is developed at the Centre for eResearch, University of Auckland. WebAssembly development by Nick Young, frontend design and development by Noel Zeng. Source code <a href="https://github.com/uoa-eresearch/MPAi/">available on GitHub</a>.</p>
+      <p>The Māori Pronunciation Aid project is led by Professor Catherine Watson, Dr Justine Hui, and Dr Peter Keegan from Waipapa Taumata Rau University of Auckland.</p>
+      <p>This web app is developed at the Centre for eResearch, University of Auckland, based on Watson et al. 2017. WebAssembly porting and backend development by Nick Young, frontend design and development by Noel Zeng. Source code <a href="https://github.com/uoa-eresearch/MPAi/">available on GitHub</a>.</p>
       <p>This project is funded by <a href="https://www.mbie.govt.nz/science-and-technology/science-and-innovation/funding-information-and-opportunities/investment-funds/curious-minds">Curious Minds He Hihiri i te Mahara</a> from the Ministry of Business, Innovation and Employment.</p>
       <p></p>
       <p>Emotiki is created by Te Puia.</p>
