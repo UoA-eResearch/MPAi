d43eb9c0fe3a3221297636e6eafecdd484026b28
Nick Young <nick.young@auckland.ac.nz>
2025-06-24 10:19:59 +1200

    move secrets to .env, add GET endpoint to retrieve zip of wavs

--- API.py
+++ API.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
 import os
-from typing import Annotated
+from typing import Annotated, Optional
 from datetime import datetime
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, File
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import StreamingResponse
+import zipfile
+import io
+from glob import glob
+import dotenv
+dotenv.load_dotenv()
 
 app = FastAPI(root_path="/MPAi_API")
 app.add_middleware(
@@ -16,7 +21,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-correct_password = open("password.txt").read().strip()
+correct_password = os.getenv("password")
+retrieval_password = os.getenv("retrieval_password")
 
 def make_safe_filename(s):
     def safe_char(c):
@@ -39,3 +45,28 @@ def upload(file: Annotated[bytes, File()], participant_id:str, password:str):
       raise HTTPException(status_code=413, detail="File too large")
   else:
     raise HTTPException(status_code=403, detail="Incorrect password")
+
+@app.get("/")
+def retrieve(password: str, participant_id: Optional[str] = None):
+    if password == retrieval_password:
+        mem_zip = io.BytesIO()
+
+        files = glob("uploads/*.wav")
+        output_filename = "wavs.zip"
+        if participant_id:
+            files = [f for f in files if participant_id in os.path.basename(f)]
+            output_filename = f"{make_safe_filename(participant_id)}_wavs.zip"
+
+        with zipfile.ZipFile(mem_zip, mode="w", compression=zipfile.ZIP_DEFLATED) as zf:
+            for file_path in files:
+                zf.write(file_path, arcname=os.path.basename(file_path))
+
+        mem_zip.seek(0)
+
+        return StreamingResponse(
+            mem_zip,
+            media_type="application/zip",
+            headers={"Content-Disposition": f"attachment; filename={output_filename}"}
+        )
+    else:
+        raise HTTPException(status_code=403, detail="Incorrect password")
